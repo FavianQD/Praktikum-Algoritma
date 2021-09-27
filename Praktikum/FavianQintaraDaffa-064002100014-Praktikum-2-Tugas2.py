c13f14e49427327a9ab23552e22c9f8c275b9c82
FavianQD <91055787+FavianQD@users.noreply.github.com>
2021-09-27 14:06:52 +0700

    Tugas 1 - Praktikum Pertemuan ke 2.
    Favian Qintara Daffa - 064002100014

--- Praktikum/FavianQintaraDaffa-064002100014-Praktikum-2-Tugas2.py
+++ Praktikum/FavianQintaraDaffa-064002100014-Praktikum-2-Tugas2.py
@@ -1,1 +1,25 @@
-#sourcecode
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 13:06:32 2021
+
+@author: Admin
+
+Nama : Favian Qintara Daffa
+NIM : 064002100014
+
+"""
+import math
+
+print("kalkulator online\n")
+a = float (input("Masukkan Nilai a : "))
+b = float (input("Masukkan Nilai b : "))
+
+print("Jawaban :\n")
+
+print("1. Jumlah A dan B adalah :\n", a+b)
+print("2. Selisih antara B dengan A adalah :\n", abs(b-a))
+print("3. Hasil kali dari A dan B adalah :\n", a*b)
+print("4. Sisa pembagian A dan B adalah :\n", a%b)
+print("5. Hasil pembagian dari A dengan B adalah :\n", a/b)
+print("6. Hasil dari log(a) adalah :\n", math.log10(a))
+print("7. Hasil dari A pangkat B adalah :\n", a**b)
